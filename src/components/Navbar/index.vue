41bd941465505831ec5cab579a5fd3139296b616
orzorzooo <orzorzooo@gmail.com>
2022-11-15 15:13:55 +0800

    git init

---
+++ src/components/Navbar/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="h-20 bg-gray-100">
+      <div class="flex h-full px-20 justify-between items-center">
+        <div class="text-2xl font-bold">APDC</div>
+        <div class="text-lg">APDC</div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/landing/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-sheet>
+      <div class="h-92vh bg-orange-400">
+        <div class="">
+          <v-img :src="bg" height="92vh">
+            <v-container class="h-full text-white">
+              <v-row class="h-full" align="center">
+                <v-col sm="12" md="6">
+                  <div class="text-6xl font-bold text-left">APDC</div>
+                  <div>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Eum, nesciunt? Id quibusdam provident consequatur dolore
+                    ipsa harum praesentium sequi odio eaque doloremque repellat,
+                    porro at ad veniam corporis. Fugiat, dolore?
+                  </div>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-img>
+        </div>
+      </div>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+import bg from "@/assets/images/landing-hero-bg.png";
+export default {
+  components: {},
+  data() {
+    return {
+      bg,
+    };
+  },
+};
+</script>
