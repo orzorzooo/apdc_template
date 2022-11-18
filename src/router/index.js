83a2654fa2334a939d2b8ca0467047bb42a29b59
orzorzooo <orzorzooo@gmail.com>
2022-11-18 11:28:26 +0800

    EDIT:router import project

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const routes = [
   {
     path: "/projects/project/:id",
     name: "project",
-    component: project,
+    component: () => import("@/views/project/index.vue"),
     props: true,
   },
   {
