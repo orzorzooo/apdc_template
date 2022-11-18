c3bbdb13c53835b5364125c76a21680d3265eb85
orzorzooo <orzorzooo@gmail.com>
2022-11-18 13:04:49 +0800

    ADD: vue-markdown

--- src/api/request.js
+++ src/api/request.js
@@ -2,17 +2,27 @@ import axios from "axios";
 export const BASEURL = `${process.env.VUE_APP_API_URL}`;
 
 // for directus封裝
-export const get = async ({ type = "items", url = "", params = {} }) => {
+export const get = async ({
+  type = "items",
+  url = "",
+  params = { fields: "*,files.*" },
+}) => {
   try {
-    const { data } = await axios.get(`${BASEURL}/${type}/${url}`, { params });
+    const { data, status } = await axios.get(`${BASEURL}/${type}/${url}`, {
+      params,
+    });
     console.log(
-      "%cREQUEST GET",
-      "border:1px solid white;padding:0.3rem 1rem;background-color:green;border-radius:1rem"
+      `%cGET ${status}`,
+      "font-weight:bold;border:1px solid white;padding:0.3rem 1rem;background-color:green;border-radius:1rem"
     );
     console.log(data);
 
     return data.data;
   } catch (error) {
+    console.log(
+      `%cGET ${error.response.status}`,
+      "font-weight:bold;border:1px solid white;padding:0.3rem 1rem;background-color:red;border-radius:1rem"
+    );
     console.log(error.message);
   }
 };
--- src/views/project/index.vue
+++ src/views/project/index.vue
@@ -1,10 +1,39 @@
 <template>
   <div>
+    <v-img
+      v-if="this.project && this.project.files.length > 0"
+      :lazy-src="
+        assetURL(this.project.files[0].directus_files_id, {
+          params: { quality: 10 },
+        })
+      "
+      :src="assetURL(this.project.files[0].directus_files_id)"
+      gradient="to top right, rgba(0,0,0,.33), rgba(0,0,0,.7)"
+      height="50vh"
+    >
+      <div class="flex justify-center items-center text-white h-full">
+        <div class="md:text-4xl font-bold">{{ project.name }}</div>
+      </div>
+    </v-img>
     <div>project {{ id }}</div>
+    <!-- {{ project.post }} -->
+    <VueMarkdown v-if="project">{{ project.post }}</VueMarkdown>
   </div>
 </template>
 <script>
+import VueMarkdown from "vue-markdown";
+import { get, assetURL } from "@/api/request";
 export default {
   props: ["id"],
+  components: { VueMarkdown },
+  data() {
+    return {
+      project: null,
+      assetURL,
+    };
+  },
+  async created() {
+    this.project = await get({ url: `projects/${this.id}` });
+  },
 };
 </script>
